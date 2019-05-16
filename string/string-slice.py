185c25510a6c2f5943d66f77073e10c3fa0d57a5
Albert-Wu <528613910@qq.com>
2019-05-16 15:37:40 +0800

    add string-slice

---
+++ string/string-slice.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+name = 'My name is Mike'
+print(name[0])
+print(name[-4])
+print(name[11:14])
+print(name[11:15])
+print(name[5:])
+print(name[:5])
--- string/txt-write.py
+++ string/txt-write.py
@@ -1,2 +1,11 @@
+# coding: utf-8
+
+num = 1
+string = '1'
+num2 = int(string)
+
 file = open('D:/用户文件/09403/桌面/1.txt', 'w')
 file.write('hello world!')
+print(num + num2)
+print(string*3)
+
